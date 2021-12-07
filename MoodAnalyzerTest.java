8db09447d71fd06a8471ea0f9ca0dd3ce9b73ebf
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-12-07 16:43:42 +0530

    Refactored to use constructors to pass message

--- MoodAnalyzerTest.java
+++ MoodAnalyzerTest.java
@@ -9,22 +9,19 @@ public class MoodAnalyzerTest {
 	
 	static MoodAnalyzer obj;            // declaring an object of MoodAnalyzer class
 
-	@BeforeClass
-	public static void instantiation() { 
-		obj = new MoodAnalyzer();       // instance of the MoodAnalyzer before the test classes execute
-	}
-	
 	@Test
 	public void testMoodAnalyzerSAD() {
 		String message = "I am in Sad Mood" ;
-		String myMood = obj.analyzingMood(message);
+		obj = new MoodAnalyzer(message);
+		String myMood = obj.analyzingMood();
 		assertEquals("SAD" , myMood);
 	}
 	
 	@Test
 	public void testMoodAnalyzerHAPPY() {
 		String message = "I am in Any Mood" ;
-		String myMood = obj.analyzingMood(message);
+		obj = new MoodAnalyzer(message);
+		String myMood = obj.analyzingMood();
 		assertEquals("HAPPY" , myMood);
 	}
 
